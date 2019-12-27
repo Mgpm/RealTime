9ff9effa87ffd11b0b3922bad3be550fabc01203
Mgpm <58890434+Mgpm@users.noreply.github.com>
2019-12-27 15:16:01 +0100

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,20 @@
+
+#Apache Kafka
+bootstrap_servers = 'localhost:9092'
+kafka_topic_name = 'myk'
+data_encoding = 'utf-8'
+
+
+"""TWITTER APP SETTINGS"""
+consumer_key    = '8SHsdaA5TSCEdGYmZl3CLFwla'
+consumer_secret = 'O6KK0074dVAHk15rZxALOdolq7Uc3WIYxASZP8HEOe1re1N4ga'
+access_token    = '404916448-R8INbuTfOryRXHodAll3HO1f6JQ4piHOK9JUTtl5'
+access_secret   = 'rufaboH6bNMzt0jiTaq9gq0O36PEzWBqRRfJBUXSGveJR'
+twitter_stream_filter = 'réforme des retraites'
+
+
+
+# sentiments analysis model settings
+model_path = "model_nlp"
+
+
